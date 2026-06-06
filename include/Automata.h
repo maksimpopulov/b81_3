37649485270414f364f8bd02bb0fbadee52ee467
maksimpopulov <viktorpopulov@gmail.com>
2026-06-06 19:23:21 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,44 +1,44 @@
 // Copyright 2026 maksimpopulov
-#ifndef AUTOMATA_H
-#define AUTOMATA_H
+#ifndef INCLUDE_AUTOMATA_H_
+#define INCLUDE_AUTOMATA_H_
 
 #include <string>
 #include <vector>
 
 enum class STATES {
-    OFF,
-    ON,
-    WAIT,
-    ACCEPT,
-    CHECK,
-    COOK,
-    CANCEL,
-    FINISH
+  OFF,
+  ON,
+  WAIT,
+  ACCEPT,
+  CHECK,
+  COOK,
+  CANCEL,
+  FINISH
 };
 
 class Automata {
-private:
-    double cash;
-    std::vector<std::string> menu;
-    std::vector<double> prices;
-    STATES state;
+ private:
+  double cash;
+  std::vector<std::string> menu;
+  std::vector<double> prices;
+  STATES state;
 
-public:
-    Automata();
-    void on();
-    void off();
-    void coin(double amount);
-    std::string getMenu();
-    STATES getState();
-    bool choice(int drinkIndex);
-    bool check(int drinkIndex);
-    void cancel();
-    void cook(int drinkIndex);
-    double finish();
-    void loadMenuFromFile(const std::string& filename);
-    void setMenu(const std::vector<std::string>& newMenu,
-        const std::vector<double>& newPrices);
-    double getCash() const;
+ public:
+  Automata();
+  void on();
+  void off();
+  void coin(double amount);
+  std::string getMenu();
+  STATES getState();
+  bool choice(int drinkIndex);
+  bool check(int drinkIndex);
+  void cancel();
+  void cook(int drinkIndex);
+  double finish();
+  void loadMenuFromFile(const std::string& filename);
+  void setMenu(const std::vector<std::string>& newMenu,
+               const std::vector<double>& newPrices);
+  double getCash() const;
 };
 
-#endif
+#endif  // INCLUDE_AUTOMATA_H_
